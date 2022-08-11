8b8b1b67b3f836dcd0d921cf140249b42ec40296
josiah <jmnyarega@gmail.com>
2022-08-11 21:50:10 +0300

    use object_set

--- src/first-class-functions/cart.ts
+++ src/first-class-functions/cart.ts
@@ -1,4 +1,4 @@
-import { ICartObject, TCart } from "../mega-mart/types";
+import { ICartObject, TCart, TField } from "../mega-mart/types";
 
 export const object_set = <T>(obj: { [k: string]: T }, k: string, v: T) => {
   const copy = Object.assign({}, obj);
@@ -8,7 +8,7 @@ export const object_set = <T>(obj: { [k: string]: T }, k: string, v: T) => {
 
 const object_keys = (obj: {}) => Object.keys(obj);
 
-export const object_remove = <T>(obj: { [k: string]: T }, k: string) => {
+export const object_remove = (obj: { [k: string]: any }, k: string) => {
   const copy = Object.assign({}, obj);
   delete copy[k];
   return copy;
@@ -33,15 +33,8 @@ export const get_free_shipping_with_item = (
   item: TCart
 ): boolean => calc_cart_total(add_item(cart, item)) >= 20;
 
-export const set_field = <V>(
-  item: TCart,
-  field: keyof TCart,
-  value: V
-): TCart => {
-  const copy = Object.assign({}, item);
-  copy[field] = value;
-  return copy;
-};
+export const set_field = (item: TCart, field: TField, value: any) =>
+  object_set(item, field, value);
 export const add_item = (cart: ICartObject, item: TCart): ICartObject =>
   object_set(cart, item.name, item);
 
@@ -50,7 +43,7 @@ export const set_field_by_name = <T>(
   name: string,
   field: keyof TCart,
   value: T
-): ICartObject => object_set(cart, name, set_field(cart[name], field, value));
+) => object_set(cart, name, set_field(cart[name], field, value));
 
 export const calc_item_total = (item: TCart) => item.price * item.quantity;
 
--- src/mega-mart/types.ts
+++ src/mega-mart/types.ts
@@ -8,6 +8,8 @@ export interface ICartObject {
   [key: string]: TCart;
 }
 
+export type TField = keyof TCart;
+
 export interface IBuyButton {
   item: TCart;
   show_free_shipping_icon: () => void;
